b6ea05076b85642ee7c2ae63840911fc49b0bbd6
larocqueg <portugalgabriel2021@gmail.com>
2025-02-11 16:39:21 +0000

    Better init.lua

--- init.lua
+++ init.lua
@@ -5,13 +5,14 @@
 --                                                    +:+ +:+         +:+     --
 --   By: gde-la-r <gde-la-r@student.42porto.com>    +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
---   Created: 2025/02/09 13:55:49 by gde-la-r          #+#    #+#             --
---   Updated: 2025/02/09 13:58:36 by gde-la-r         ###   ########.fr       --
+--   Created: 2025/02/11 16:36:49 by gde-la-r          #+#    #+#             --
+--   Updated: 2025/02/11 16:36:51 by gde-la-r         ###   ########.fr       --
 --                                                                            --
 -- ************************************************************************** --
 
+-- General settings
 vim.cmd("syntax on")
-vim.cmd("set nu")
+vim.cmd("set number")
 vim.cmd("set mouse=a")
 vim.cmd("set si")
 vim.cmd("set wrap")
@@ -23,6 +24,7 @@ vim.cmd("set autoindent")
 vim.cmd("set smarttab")
 vim.g.mapleader = " "
 
+-- Auto commands for C & H files (ensuring no expandtab)
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "c", "h" },
     callback = function()
@@ -33,57 +35,7 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-                "git",
-                "clone",
-                "--filter=blob:none",
-                "https://github.com/folke/lazy.nvim.git",
-                lazypath,
-        })
-end
-vim.opt.rtp:prepend(lazypath)
-
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin" },
-    { 
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        "Diogo-ss/42-header.nvim",
-        cmd = { "Stdheader" },  -- Command to trigger the plugin
-        keys = { "<F1>" },      -- Map the plugin to <F1>
-        opts = {
-            default_map = true,  -- Default mapping for <F1>
-            auto_update = true,  -- Auto update header on save
-            user = "gde-la-r",   -- Your username
-            mail = "gde-la-r@student.42porto.com", -- Your email
-        },
-        config = function(_, opts)
-            require("42header").setup(opts)  -- Setup the plugin with options
-        end,
-    },
-    {
-            "kylechui/nvim-surround",
-            version = "*",
-            event = "VeryLazy",
-            opts = {},
-    },
-}
-
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set number")
-vim.cmd("syntax on")
-vim.cmd("set mouse=a")
-vim.g.mapleader = " "
-
+-- Lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -96,6 +48,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Plugins
 local plugins = {
     { "catppuccin/nvim", name = "catppuccin" },
     { 
@@ -109,8 +62,8 @@ local plugins = {
         opts = {
             default_map = true,
             auto_update = true,
-            user = "username",
-            mail = "your@email.com",
+            user = "gde-la-r",   -- Your username
+            mail = "gde-la-r@student.42porto.com", -- Your email
         },
         config = function(_, opts)
             require("42header").setup(opts)
@@ -118,30 +71,21 @@ local plugins = {
     },
     {
         "kylechui/nvim-surround",
-        config = function()
-            require("nvim-surround").setup({
-                -- Add any custom configurations here
-            })
-        end
+        event = "VeryLazy",
+        opts = {},
     },
     {
         "folke/trouble.nvim",
-        opts = {},  -- Default options
-        cmd = "Trouble",  -- Command to trigger Trouble
+        cmd = "Trouble",
         keys = {
             { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
             { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
             { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
-            { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / references / ... (Trouble)" },
+            { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "LSP Definitions / References" },
             { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
             { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
         },
-        config = function()
-            require("trouble").setup({
-                -- Custom options can be added here if needed
-                -- e.g., use_lsp_diagnostics = true
-            })
-        end
+        opts = {},
     }
 }
 
